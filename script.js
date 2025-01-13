b5a7624a5e314514a94012a4cb8ed374f3f4a3ee
priyansh-24 <gunutig123@gmail.com>
2025-01-13 13:03:44 +0530

    sound inc

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ audioToggle.style.display = 'none';
 let isDragging = false;
 let offsetX = 0; 
 let offsetY = 0;
-audio.volume = 0.1;  
+audio.volume = 0.3;  
 
 audio.pause();
 // Dragging with the mouse
